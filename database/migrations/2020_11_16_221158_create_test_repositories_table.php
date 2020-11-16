731d936fc10f4b14aae5c331e71695d9cc4dc5fb
joao.silva <joao.silva@arapiraca.al.gov.br>
2020-11-16 20:26:06 -0300

    Enviado para o repositorio remoto

---
+++ database/migrations/2020_11_16_221158_create_test_repositories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateTestRepositoriesTable.
+ */
+class CreateTestRepositoriesTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('test_repositories', function(Blueprint $table) {
+            $table->increments('id');
+
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('test_repositories');
+	}
+}
